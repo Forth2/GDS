836a1420eeda6ba359d0a022f86338d24a612ca0
zhaoshanshan0906 <2410002148@qq.com>
2017-11-27 16:49:10 +0800

    赵姗杉   2017-11-27

---
+++ M/app/src/main/java/bawei/com/gds_project/fragment/FragmentMine.kt
@@ -0,0 +1,18 @@
+package bawei.com.gds_project.fragment
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ *  1.类的用途
+ *  2.@author棒棒糖：赵姗杉
+ *  3.@date2017/11/27  16：46
+ */
+class FragmentMine :Fragment() {
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
