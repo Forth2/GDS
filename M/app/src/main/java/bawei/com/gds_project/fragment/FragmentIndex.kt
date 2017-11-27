bfd141629d0a58214debc779df99e489a811cd7d
Aries10 <1374960689@qq.com>
2017-11-27 19:30:34 +0800

    color文件

--- M/app/src/main/java/bawei/com/gds_project/fragment/FragmentIndex.kt
+++ M/app/src/main/java/bawei/com/gds_project/fragment/FragmentIndex.kt
@@ -2,9 +2,12 @@ package bawei.com.gds_project.fragment
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import bawei.com.gds_project.R
 
 /**
  * Created by dell-pc on 2017/11/27.
@@ -13,9 +16,14 @@ class FragmentIndex : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
+        val view = inflater!!.inflate(R.layout.fragment_index, container, false)
+
+        val index_title = view.findViewById<TextView>(R.id.index_title)
+        val rv_index = view.findViewById<RecyclerView>(R.id.rv_index)
+        
 
 
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return view
     }
 
 }
