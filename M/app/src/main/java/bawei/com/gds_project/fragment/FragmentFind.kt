69792b1a174a426d5f4f313c38997e9e9d519592
douyahaha <1372943215@qq.co>
2017-11-28 16:22:20 +0800

    F

--- M/app/src/main/java/bawei/com/gds_project/fragment/FragmentFind.kt
+++ M/app/src/main/java/bawei/com/gds_project/fragment/FragmentFind.kt
@@ -7,17 +7,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import bawei.com.gds_project.R
+import bawei.com.gds_project.find.FindView
 
 
 /**
  * Created by 杨圆圆 on 2017/11/27.
  */
-class FragmentFind:Fragment() {
+class FragmentFind:Fragment(),FindView {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v = inflater!!.inflate(R.layout.fragment_find, container, false)
         val recycle_find = v.findViewById<RecyclerView>(R.id.recycle_find)
 
         return v
+    }
+    override fun getBean() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+
     }
 
 }
