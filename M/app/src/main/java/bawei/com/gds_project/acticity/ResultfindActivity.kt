b097dc464cd22e70852a328d9164af5e2919ddb8
douyahaha <1372943215@qq.co>
2017-11-29 20:25:25 +0800

    Find

--- M/app/src/main/java/bawei/com/gds_project/acticity/ResultfindActivity.kt
+++ M/app/src/main/java/bawei/com/gds_project/acticity/ResultfindActivity.kt
@@ -2,18 +2,21 @@ package bawei.com.gds_project.acticity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
+import android.support.v7.widget.LinearLayoutManager
 import android.widget.Toast
 import bawei.com.gds_project.R
+import bawei.com.gds_project.adapter.FindResultAdapter
 import bawei.com.gds_project.bean.FindSonBean
 import bawei.com.gds_project.findresult.FindResultPresenterImpl
 import bawei.com.gds_project.findresult.FindResultView
+import kotlinx.android.synthetic.main.activity_resultfind.*
 
 class ResultfindActivity : AppCompatActivity(),FindResultView {
     var findResultPresenter:FindResultPresenterImpl?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultfind)
+
         //获取传过来的值
         val name = intent.getStringExtra("name")
         Toast.makeText(this,"haha"+name,Toast.LENGTH_SHORT).show()
@@ -23,10 +26,12 @@ class ResultfindActivity : AppCompatActivity(),FindResultView {
         findResultPresenter= FindResultPresenterImpl(this)
         //调用Presenter对象
         findResultPresenter?.getVideoData(name)
+        tv_findResultTitle.setText(name)
     }
     override fun getData(bean: FindSonBean) {
-        Log.i("xxx",bean.toString())
-
+        var findResultAdapter=FindResultAdapter(bean,this@ResultfindActivity)
+        recycle_findResult.layoutManager=LinearLayoutManager(this@ResultfindActivity)
+        recycle_findResult.adapter=findResultAdapter
     }
 
 }
---
+++ M/app/src/main/java/bawei/com/gds_project/adapter/FindResultAdapter.kt
@@ -0,0 +1,48 @@
+package bawei.com.gds_project.adapter
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import bawei.com.gds_project.R
+import bawei.com.gds_project.bean.FindSonBean
+import com.bumptech.glide.Glide
+
+/**
+ * Created by 杨圆圆 on 2017/11/29.
+ */
+class FindResultAdapter(mBean:FindSonBean,mContext:Context): RecyclerView.Adapter<FindResultAdapter.MyViewHolder>() {
+
+
+
+    private var bean:FindSonBean?=mBean
+    private var context:Context?=mContext
+
+    override fun getItemCount(): Int {
+        return bean!!.itemList!!.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder?, position: Int) {
+        var list: List<FindSonBean.ItemListBean>? =bean!!.itemList
+        if(list?.get(position)!!.type.equals("banner2")){
+            return
+        }
+        holder?.tv_findResult?.setText(list?.get(position)!!.data!!.title)
+
+        Glide.with(context).load(list?.get(position)!!.data!!.cover!!.detail).into(holder?.iv_findResult)
+    }
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): MyViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.findresult_item, parent, false)
+        var holder=MyViewHolder(view)
+        return holder
+    }
+
+
+    class MyViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+        var tv_findResult:TextView=itemView!!.findViewById(R.id.tv_findResult)
+        var iv_findResult:ImageView=itemView!!.findViewById(R.id.iv_findResult)
+    }
+}
